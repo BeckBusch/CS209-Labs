d5d50eaa1348db414271251b83f1e2897c7b8fcf
Beck Busch <beckbusch5@gmail.com>
2021-09-24 18:37:37 +1200

    Finished Task 4.5

--- Lab4_ADC/Main Lab Task/Main Lab Task/main.c
+++ Lab4_ADC/Main Lab Task/Main Lab Task/main.c
@@ -1,9 +1,9 @@
 /*
- * Main Lab Task.c
- *
- * Created: 20/09/2021 12:59:27 pm
- * Author : GGPC
- */ 
+* Main Lab Task.c
+*
+* Created: 20/09/2021 12:59:27 pm
+* Author : GGPC
+*/
 
 #define F_CPU 2000000
 
@@ -26,26 +26,48 @@ int main(void)
 	adc_init();
 	
 	char formatText[8];
-	sprintf(formatText, "%s", "mV");
+	sprintf(formatText, "%s", ", ");
 	
-    while (1) {
-		uint16_t adcReturn = adc_read(2);
+	uint16_t pin0Array[40]; uint16_t pin1Array[40];
+	
+	
+	for(int count=0; count < 40; count++){
+		uint16_t adcReturn = adc_read(0);
 		uint16_t adcReadmv = adc_convert_mv(adcReturn);
+		pin0Array[count] = adcReadmv;
 		
+		adcReturn = adc_read(1);
+		adcReadmv = adc_convert_mv(adcReturn);
+		pin1Array[count] = adcReadmv;
+	}
+	
+	for(int count=0; count < 40; count++){
 		char string_buffer[8];
-		sprintf(string_buffer, "%d", adcReadmv);
-
+		sprintf(string_buffer, "%d", pin0Array[count]);
+		
 		for (int i=0; i < strlen(string_buffer); i++){
-			usart_transmit(string_buffer[i]);	
+			usart_transmit(string_buffer[i]);
 		}
+		
 		for (int i=0; i < strlen(formatText); i++){
 			usart_transmit(formatText[i]);
 		}
+	}
+	
+	usart_transmit(NEWLINE);
+	usart_transmit(RETURN);
+	
+	for(int count=0; count < 40; count++){
+		char string_buffer[8];
+		sprintf(string_buffer, "%d", pin1Array[count]);
 		
-		usart_transmit(NEWLINE);
-		usart_transmit(RETURN);
+		for (int i=0; i < strlen(string_buffer); i++){
+			usart_transmit(string_buffer[i]);
+		}
 		
-		_delay_ms(1000);
-    }
+		for (int i=0; i < strlen(formatText); i++){
+			usart_transmit(formatText[i]);
+		}
+	}
 }
 
