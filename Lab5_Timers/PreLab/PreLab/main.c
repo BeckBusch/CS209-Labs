bad45301a18e1092b3737223d331adf08193852c
Beck Busch <beckbusch5@gmail.com>
2021-09-26 20:19:36 +1300

    Pre lab finished

---
+++ Lab5_Timers/PreLab/PreLab/main.c
@@ -0,0 +1,28 @@
+/*
+ * PreLab.c
+ *
+ * Created: 26/09/2021 3:25:04 pm
+ * Author : GGPC
+ */ 
+
+#define F_CPU 2000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void) {
+	// Port B Output
+	DDRB = 0;
+	// Port C&D Input
+	DDRC = 1;
+	DDRD = 1;
+
+    while (1) {
+		PORTB |= (1<<PB5); // Turn on
+		_delay_ms(375); // Literally don't understand why I need to double it but...
+		PORTB &= ~(1<<PB5); // Turn off
+		_delay_ms(125);
+    }
+}
+
